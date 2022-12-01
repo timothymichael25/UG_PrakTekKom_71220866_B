271d3bda33ea03585f86e9cb233c3448ba45bb57
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2022-12-01 13:26:18 +0700

    Add files via upload

--- Soal_1.py
+++ Soal_1.py
@@ -1,31 +1,32 @@
-def add():
-    A = float(input("Masukan angka pertama: "))
-    B = float(input("Masukan angka kedua: "))
-    add = A + B
-    print(A, "+" , B, add)
-
-add()
-
-def subtract():
-    A = float(input("Masukan angka pertama: "))
-    B = float(input("Masukan angka kedua: "))
-    subtract = A - B
-    print(A, "-" , B, subtract)
-
-subtract()
-
-def multiply():
-    A = float(input("Masukan angka pertama: "))
-    B = float(input("Masukan angka kedua: "))
-    multiply = A * B
-    print(A, "*", B, multiply)
-
-multiply()
-
-def devide():
-    A = float(input("Masukan angka pertama: "))
-    B = float(input("Masukan angka kedua: "))
-    devide = A / B
-    print(A, "/" , B , devide)
-
-devide()
+while True:
+	print("----------------------------------")
+	print("Masukkan '1' untuk penjumlahan")
+	print("Masukkan '2' untuk pengurangan")
+	print("Masukkan '3' untuk perkalian")
+	print("Masukkan '4' untuk pembagian")
+	print("Masukkan '0' untuk *keluar*")
+	user_input = int(input("pilihan: "))
+	if user_input == 0:
+		break
+	elif user_input == 1:
+		angka1 = float(input("angka-1 : "))
+		angka2 = float(input("angka-2 : "))
+		hasil = angka1 + angka2
+		print(">>> Hasil = {}".format(hasil))
+	elif user_input == 2:
+		angka1 = float(input("angka-1 : "))
+		angka2 = float(input("angka-2 : "))
+		hasil = angka1 - angka2
+		print(">>> Hasil = {}".format(hasil))
+	elif user_input == 3:
+		angka1 = float(input("angka-1 : "))
+		angka2 = float(input("angka-2 : "))
+		hasil = angka1 * angka2
+		print(">>> Hasil = {}".format(hasil))
+	elif user_input == 4:
+		angka1 = float(input("angka-1 : "))
+		angka2 = float(input("angka-2 : "))
+		hasil = angka1 / angka2
+		print(">>> Hasil = {}".format(hasil))
+	else:
+		print(">>> Angka yang anda masukkan salah!")
