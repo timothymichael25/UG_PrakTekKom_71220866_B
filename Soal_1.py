76bd9a6f7bd70112bbbc5de02909a0fa2304cb62
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2022-12-01 13:07:40 +0700

    Add files via upload

---
+++ Soal_1.py
@@ -0,0 +1,31 @@
+def add():
+    A = float(input("Masukan angka pertama: "))
+    B = float(input("Masukan angka kedua: "))
+    add = A + B
+    print(A, "+" , B, add)
+
+add()
+
+def subtract():
+    A = float(input("Masukan angka pertama: "))
+    B = float(input("Masukan angka kedua: "))
+    subtract = A - B
+    print(A, "-" , B, subtract)
+
+subtract()
+
+def multiply():
+    A = float(input("Masukan angka pertama: "))
+    B = float(input("Masukan angka kedua: "))
+    multiply = A * B
+    print(A, "*", B, multiply)
+
+multiply()
+
+def devide():
+    A = float(input("Masukan angka pertama: "))
+    B = float(input("Masukan angka kedua: "))
+    devide = A / B
+    print(A, "/" , B , devide)
+
+devide()
